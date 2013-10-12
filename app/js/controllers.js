c6c470e6fdc87a90e2e7c9dbd4d3a9ea2b5158cc
doddyth <doddytrihutomo@gmail.com>
2013-10-12 23:07:30 +0700

    using bootstrap 3

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,13 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
-
+  controller('NavBarCtrl', ['$scope', '$location', function($scope, $location) {
+	$scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+  }])
+  .controller('MyCtrl1', ['$scope', function($scope) {
+	$scope.data = "ini halaman home";
   }])
   .controller('MyCtrl2', [function() {
 
